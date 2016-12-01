64132caae5253904130ff0dd8a96e0492bf37cc9
Anudle <AnuShrestha007@gmail.com>
2016-12-01 14:12:24 -0700

    baby routes added

--- routes/index.js
+++ routes/index.js
@@ -1,20 +1,55 @@
 var express = require('express');
 var router = express.Router();
 var knex = require('../db/knex')
+var query = require('../db/query')
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: '52Finds' });
 });
 
+router.get('/map', function(req, res, next) {
+  res.render('map', { title: 'F2Finds' });
+});
+
+
+
 router.get('/state/:id', function(req, res, next) {
-    knex('states').where({
-        id: req.params.id
-    }).then(function(data) {
+    knex.from('states').innerJoin('advisors', 'states.id', 'advisors.stateid').where({
+        stateid: req.params.id
+    })
+
+    .then(function(data) {
         res.render('state', {
             data: data[0]
         });
     });
 });
+
+
+router.get('/directs/:id', function(req, res, next) {
+  knex.from('states').innerJoin('directs', 'states.id', 'directs.stateid').where({
+      stateid: req.params.id
+  })
+
+  .then(function(data) {
+      res.render('directs', {
+          data: data[0]
+      });
+  });
+});
+
+router.get('/advisors/:id', function(req, res, next) {
+  knex.from('states').innerJoin('advisors', 'states.id', 'advisors.stateid').where({
+      stateid: req.params.id
+  })
+
+  .then(function(data) {
+      res.render('advisors', {
+          data: data[0]
+      });
+  });
+});
+
 
 module.exports = router;
